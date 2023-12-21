a5c0a90cd3e50857b4ee8f06de3c917151e489a5
Thedarkness-bd <toisiftasrik@gmail.com>
2023-12-22 05:04:16 +0600

    Custom not found page created and used it

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-8xl mt-[200px] text-red-800">404 Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
